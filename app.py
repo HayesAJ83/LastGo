083071ade3044849f954e0ea3238dcba7a55aafb
HayesAJ83 <72214809+HayesAJ83@users.noreply.github.com>
2021-05-08 18:06:28 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__, static_url_path='/assets', static_folder='assets',
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('pages/index.html')
 
 if __name__ == "__main__":
     app.run(Debug=True)
